ba046cef23e7e54490a243bb7e92092c3e5d5415
Daniel D Demerin <ddemerin@gmail.com>
2020-05-25 10:46:39 -0400

    running through Tania Rascia's react tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 
 const App = () => {
-  const [name, setName] = useState('')
-  console.log(name)
-
-  const updateName = (e) => {
-    e.preventDefault()
-    const key = e.target.name
-    console.log(key)
-    const value = e.target.value
-    console.log(value)
-    setName(value)
-  }
-
   return (
-    <>
-      <h1>Enter your name below!</h1>
-      <form>
-        <input
-          type="text"
-          placeholder="Your Name Here"
-          name="Name"
-          value={name}
-          onChange={updateName}
-        />
-        <button onClick={updateName}>Submit</button>
-        {{ name } ? <h2>{`Welcome ${name}!`}</h2> : <></>}
-      </form>
-    </>
+    <div className="App">
+      <h1>Hello, React!</h1>
+    </div>
   )
 }
 
