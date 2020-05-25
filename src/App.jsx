9f1f0ee3c13f45660a14c4e07353001662a47a73
Daniel D Demerin <ddemerin@gmail.com>
2020-05-25 11:45:53 -0400

    added remove function to table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
 import Table from './components/Table'
 
-const App = () => {
-  return (
-    <div className="container">
-      <Table />
-    </div>
-  )
+class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspiring Actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state
+
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
